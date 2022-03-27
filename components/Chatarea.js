fa93a712f0622bb1ab2a2dbf635e793cc07dd85b
Nisarg Chaudhari <nisrg5601@gmail.com>
2022-03-28 00:24:59 +0530

    deployment error fix

--- components/Chatarea.js
+++ components/Chatarea.js
@@ -6,7 +6,7 @@ export default function Chatarea() {
     const i = 0
     return(
         <div id="chatarea" className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
-                { chat.map((m) => <Msg sender={m["sender"]} msg= {m["msg"]} i={i++}/> ) }        
+                { chat.map((m) => <Msg key={m.id} sender={m["sender"]} msg= {m["msg"]} i={i++}/> ) }        
         </div>
     );
 }
